480f2ba7398a4b8878c7f1e3e5dd705431a3d320
riccardo <106812074+riccardo-gnosis@users.noreply.github.com>
2023-06-06 14:20:33 +0200

    devnet

--- packages/contracts-bedrock/deploy-config/gnosispay-devnet.ts
+++ packages/contracts-bedrock/deploy-config/gnosispay-devnet.ts
@@ -1,4 +1,4 @@
 import { DeployConfig } from '../src/deploy-config'
-import config from './gnosispay-testnet.json'
+import config from './gnosispay-devnet.json'
 
 export default config satisfies DeployConfig
